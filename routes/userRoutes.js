5b21945d22ff186446f283d959a209449596cf4d
Hom Dahal <homdahal997@gmail.com>
2024-06-28 17:44:15 -0400

    Some file rename and update routes for each

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const usersCtrl = require('../controllers/users.js');
+const usersCtrl = require('../controllers/userController.js');
 
 const router = Router();
 
