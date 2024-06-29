3460bd1bd42f67db7b596946b3fe72684be99a72
Hom Dahal <homdahal997@gmail.com>
2024-06-28 20:18:08 -0400

    some update for user schema

--- models/userModel.js
+++ models/userModel.js
@@ -1,26 +1,26 @@
 const { Schema, model } = require('../config/db-connection.js');
 
-const userSchema = Schema({
+const userSchema = new Schema({
   name: {
     type: String,
-    required: true
+    required: [true, 'Name is required']
   },
   email: {
     type: String,
-    required: true,
-    unique: true
+    required: [true, 'Email is required'],
+    unique: true,
   },
   password: {
     type: String,
-    required: true
+    required: [true, 'Password is required']
   },
   avatar: {
-    type: String
+    type: String,
   },
   date: {
     type: Date,
     default: Date.now
   }
-});
+}, { timestamps: true }); // Use timestamps to automatically add `createdAt` and `updatedAt` fields
 
-module.exports = model('User', userSchema);
+module.exports = model('User', userSchema, 'users'); // Explicitly defining the collection name
