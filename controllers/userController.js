0eb124895d7c5b085130cac4aa0758f74a9d3f96
Hom Dahal <homdahal997@gmail.com>
2024-07-20 10:54:53 -0400

    refactor: Update user routes and add avatar functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,12 @@
-const User = require('../models/userModel.js');
-const { check, validationResult } = require('express-validator');
+const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const gravatar = require('gravatar');
+const config = require('config');
+const { validationResult } = require('express-validator');
 
-// Validation middleware
-const validateUser = [
-  check('name', 'Name is required').notEmpty(),
-  check('email', 'Please include a valid email').isEmail(),
-  check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
-];
+const User = require('../models/userModel');
 
-async function createUser(req, res) {
+exports.createUser = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
@@ -28,7 +23,14 @@ async function createUser(req, res) {
 
     const normalizeUrl = (await import('normalize-url')).default;
 
-    const avatar = normalizeUrl(gravatar.url(email, { s: '200', r: 'pg', d: 'mm' }), { forceHttps: true });
+    const avatar = normalizeUrl(
+      gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      }),
+      { forceHttps: true }
+    );
 
     user = new User({
       name,
@@ -48,28 +50,17 @@ async function createUser(req, res) {
       }
     };
 
-    const token = await new Promise((resolve, reject) => {
-      jwt.sign(
-        payload,
-        process.env.JWT_SECRET, 
-        { expiresIn: '5 days' },
-        (err, token) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(token); 
-          }
-        }
-      );
-    });
-    res.json({token});
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: '5 days' },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
   }
-}
-
-module.exports = {
-  validateUser,
-  createUser,
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,17 @@
 const { Router } = require('express');
-const usersCtrl = require('../controllers/userController.js');
+const { check } = require('express-validator');
+const usersCtrl = require('../controllers/userController');
 
 const router = Router();
 
-router.post('/', usersCtrl.createUser);
+router.post(
+    '/',
+    [
+        check('name', 'Name is required').notEmpty(),
+        check('email', 'Please include a valid email').isEmail(),
+        check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+    ],
+    usersCtrl.createUser
+);
 
 module.exports = router;
